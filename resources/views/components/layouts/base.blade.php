7f0aaabefba2db5c1e7b756e8dcb53bf7e135de0
Dan Harrin <git@danharrin.com>
2021-11-15 11:35:24 +0000

    wip

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -17,14 +17,11 @@
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=JetBrains+Mono&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet" />
 
-        <link rel="stylesheet" href="{{ route('filament.asset', [
-            'id' => \Filament\get_asset_id('app.css'),
-            'path' => 'app.css',
-        ]) }}" />
+        <link rel="stylesheet" href="{{ \Filament\Facades\Filament::getThemeUrl() }}" />
 
-        @foreach (\Filament\Facades\Filament::getStyles() as $path)
+        @foreach (\Filament\Facades\Filament::getStyles() as $name => $path)
             @if (Str::of($path)->startsWith(['http://', 'https://']))
                 <link rel="stylesheet" href="{{ $path }}" />
             @else
