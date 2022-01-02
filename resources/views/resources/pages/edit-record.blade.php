6530259743425d00ebaa8136ae110e943deaaf03
Dan <dan@danharrin.com>
2022-01-02 23:51:18 +0000

    chore: styling

--- resources/views/resources/pages/edit-record.blade.php
+++ resources/views/resources/pages/edit-record.blade.php
@@ -1,11 +1,11 @@
-<x-filament::page>
+<x-filament::page :widget-record="$record">
     <x-filament::form wire:submit.prevent="save">
         {{ $this->form }}
 
         <x-filament::form.actions :actions="$this->getFormActions()" />
     </x-filament::form>
 
-    @if (count($relationManagers = $this->getResource()::getRelations()))
+    @if (count($relationManagers = $this->getRelationManagers()))
         <x-filament::hr />
 
         <x-filament::resources.relation-managers :active-manager="$activeRelationManager" :managers="$relationManagers" :owner-record="$record" />
--- resources/views/resources/pages/view-record.blade.php
+++ resources/views/resources/pages/view-record.blade.php
@@ -1,3 +1,9 @@
-<x-filament::page>
+<x-filament::page :widget-record="$record">
     {{ $this->form }}
+
+    @if (count($relationManagers = $this->getRelationManagers()))
+        <x-filament::hr />
+
+        <x-filament::resources.relation-managers :active-manager="$activeRelationManager" :managers="$relationManagers" :owner-record="$record" />
+    @endif
 </x-filament::page>
