caadc3852c4b68ffed82df56f2ff10d90209cdd8
Dan <dan@danharrin.com>
2022-04-07 17:15:46 +0000

    Merge branch 2.x of https://github.com/laravel-filament/filament into 2.x

--- src/FilamentServiceProvider.php
+++ src/FilamentServiceProvider.php
@@ -47,6 +47,7 @@ class FilamentServiceProvider extends PackageServiceProvider
             Commands\MakeHasManyCommand::class,
             Commands\MakeHasManyThroughCommand::class,
             Commands\MakeMorphManyCommand::class,
+            Commands\MakeMorphToManyCommand::class,
             Commands\MakePageCommand::class,
             Commands\MakeResourceCommand::class,
             Commands\MakeUserCommand::class,
@@ -71,7 +72,13 @@ class FilamentServiceProvider extends PackageServiceProvider
 
     public function packageRegistered(): void
     {
-        $this->app->singleton('filament', function (): FilamentManager {
+        $this->app->resolving('filament', function (): void {
+            $this->discoverPages();
+            $this->discoverResources();
+            $this->discoverWidgets();
+        });
+
+        $this->app->scoped('filament', function (): FilamentManager {
             return app(FilamentManager::class);
         });
 
@@ -79,10 +86,6 @@ class FilamentServiceProvider extends PackageServiceProvider
         $this->app->bind(LogoutResponseContract::class, LogoutResponse::class);
 
         $this->mergeConfigFrom(__DIR__ . '/../config/filament.php', 'filament');
-
-        $this->discoverPages();
-        $this->discoverResources();
-        $this->discoverWidgets();
     }
 
     public function packageBooted(): void
